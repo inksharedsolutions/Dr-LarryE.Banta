4b9ce69416e85709f354ea9e97b4383f79e174e0
InkSharedSolutions <webdev.inkshared@gmail.com>
2021-09-15 16:20:07 +0800

    Updtes"

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,8 +41,8 @@ module.exports = {
               "excerpt_separator": `<!-- endexcerpt -->`
           }
       },
-      // this (optional) plugin enables Progressive Web App + Offline functionality
-      // To learn more, visit: https://gatsby.dev/offline
-      // `gatsby-plugin-offline`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
